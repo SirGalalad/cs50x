62bc0d776ce57d60f597c8067e3ea8397f97956a
SirGalalad <elresoldelhonor@protonmail.com>
2022-06-22 11:46:57 -0400

    Update with Comments on how to format arguments
    I've placed a lot of comments with the different ways in which you can structure the arguments or functions to achieve the same results.

--- hello.py
+++ hello.py
@@ -1,7 +1,20 @@
 # Ask user for their name
 name = input("What's your name? ")
 
-# Say hello to user
-print("hello,")
-print(name)
+# Say hello to user 
+"""
+This seems to be a cool little comment type as well
+"""
 
+print("hello, ", end="")
+print(name)
+# print("hello,", name)
+# print(name) I'm taking this out to try and get the output onto the same line to clean things up. I do that above by putting "+ name" after the "hello". I can also rewrite this function as: print("hello, ", name)
+
+# It should be written as: print("hello,", name) specifically WITHOUT the space after hello since the placement of 2 or more arguments together automatically spaces the outputs.
+
+# docs.python.org/3/library/functions is the official documentation
+
+# Yet another way that we can write the function and have it give the output on one line is like this: print("hello, ", end="") followed on the next line by: print(name)  This makes it so that the default value of end="/n" which means "end each function with a new line" gets overridden 
+
+# Double quotes "" vs single quotes '' doesn't matter in python, what matters is consistency. Unless you're trying to get quotes in the output. Then use different quotes for things. ie print('hello, "friend"') or even: print("hello, \"friend\"")
