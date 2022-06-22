096ae028f53f639cab1061f84fe731b567f6611d
SirGalalad <elresoldelhonor@protonmail.com>
2022-06-22 13:12:05 -0400

    Edit caculator to perform different operations
    Here we changed how to write the functions if we're doing different mathematical operations, ie division. We also had an example of writing with fstrings vs normal functions and arguments.

--- caculator.py
+++ caculator.py
@@ -1,7 +1,18 @@
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+x = float(input("What's x? "))
+y = float(input("What's y? "))
 
 # z = x + y #this doesn't work because "+" concatenates 
 # z = int(x) + int(y)
 
-print(x + y)
+# z = round(x + y, 3)
+# print(f"{z:,}")
+
+
+# practicing using different mathematic operations
+z = round(x / y, 2)
+
+print(z)
+
+# another way to right the above function is z = x / y followed by print(f"{z:2f}") if you want to use fstrings
+
+# float vs int makes the output 3.0 vs just 3, but it still treats all of the inputs as actual integers
