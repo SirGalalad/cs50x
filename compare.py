348a5147be61ce9e836c3715e4d2c2421c33af14
SirGalalad <elresoldelhonor@protonmail.com>
2022-06-26 16:56:13 -0400

    Add "equal to" "or" & "not equal to" expressions
    Here we practiced using the other expression listed, including "or" to see how the program can be written.

--- compare.py
+++ compare.py
@@ -1,10 +1,24 @@
+from ctypes.wintypes import PINT
+
+
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
 # The following is a BOOLEAN expression
-if x < y: 
-    print("x is less than y")
-elif x > y:
-    print("x is greater than y")
-else:
+# if x < y: 
+#     print("x is less than y")
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x is equal to y")
+
+# if x < y or x > y:
+# if x != y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+if x == y:
     print("x is equal to y")
+else:
+    print("x is not equal to y")
